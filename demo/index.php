359f4fe998d60160b42e112bbc033b9f334817d2
Manuel Marquez <marquezmanu@gmail.com>
2014-01-18 20:12:57 -0300

    add footer

--- demo/index.php
+++ demo/index.php
@@ -32,6 +32,11 @@
 			<a href="https://github.com/marquezmanu/FileUtility"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
 			<div class="spacer" style="clear: both;"></div>
 		</div>
+		<footer>
+			<div class="footer-container">
+				<a href="http://www.000webhost.com/" target="_blank"><img src="http://www.000webhost.com/images/80x15_powered.gif" alt="Web Hosting" width="80" height="15" border="0" /></a>
+			</div>
+		</footer>
 	</body>
 
 	<script>
